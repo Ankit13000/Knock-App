dca73be32ecd649f1c4da07eb9e822d703663a44
Ankit Verma <ankitraj13000@gmail.com>
2025-07-09 11:52:15 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/onboarding/OnboardingCarousel.tsx
+++ src/components/onboarding/OnboardingCarousel.tsx
@@ -26,7 +26,7 @@ const onboardingSteps = [
     description: 'Challenge your skills and compete for real cash prizes.',
   },
   {
-    image: 'https://placehold.co/600x400.png',
+    image: 'https://images.unsplash.com/photo-1522069213448-443a614da9b6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHxnYW1lfGVufDB8fHx8MTc1MjA0OTcyN3ww&ixlib=rb-4.1.0&q=80&w=1080',
     imageHint: 'start competing',
     title: 'Start Competing Now!',
     description: 'Join thousands of players and climb the leaderboards.',
